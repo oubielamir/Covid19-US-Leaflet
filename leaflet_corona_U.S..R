64a5b4f658272c7c79248df7be82efbe34c49256
oubielamir <oubielamir@Oubis-MacBook-Pro.local>
2020-05-29 13:25:32 -0400

    Updated cases and deaths numbers + fixed the issue with the circle radius. SliderInput added for cases.

--- leaflet_corona_U.S..R
+++ leaflet_corona_U.S..R
@@ -5,7 +5,7 @@ library(readr)
 library(tidyverse)
 
 
-covid <- read_csv("04-28-2020.csv")
+covid <- read_csv("05-27-2020.csv")
 
 covid <- covid %>% rename(lng  = Long_, lat = Lat)
 
@@ -13,9 +13,12 @@ covid <- covid %>% rename(lng  = Long_, lat = Lat)
 
 ui <- bootstrapPage(
   
-leafletOutput('map', width = "200%", height = 1000)  
+leafletOutput('map', width = "200%", height = 1000) ,
+
+absolutePanel(top =10, right = 30, id = 'controls', 
+              sliderInput('cases', 'Number of cases', 1, 500000, 3000)
   
-  
+)
   
 )
 
@@ -24,12 +27,15 @@ leafletOutput('map', width = "200%", height = 1000)
 server <- function(input, output, session){
 
   output$map <-  renderLeaflet({
-covid %>%
+covid %>% filter(Confirmed <= input$cases) %>%
 #%>% filter(Deaths <= 100) 
  leaflet() %>% addProviderTiles(providers$Stamen.Toner) %>%
   #fitBounds(~min(covid$Lat), ~min(covid$Long_), ~max(covid$Lat), ~max(covid$Long_)) %>%
-  setView(-98.58, 39.82, zoom = 1) %>% addCircleMarkers(radius = ~(Confirmed/1000), weight = 1,
-                                            popup = paste(covid$Province_State, '\n', (as.character(covid$Confirmed)), 'cases'))
+  setView(-95.7129, 37.0902, zoom = 5) %>% addCircleMarkers( weight = ~(Deaths/1000),
+                                popup = paste(covid$Province_State, '\n', (as.character(covid$Confirmed)), 'cases, ',
+                                                        as.character(covid$Deaths), 'deaths')
+                                         
+     )
 })
     
 }
