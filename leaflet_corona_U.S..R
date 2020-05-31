558132c03496a00560fe38d77ff8815ba5b8ffce
oubielamir <oubielamir@Oubis-MacBook-Pro.local>
2020-05-31 11:41:17 -0400

    Fixed popup issue. States with more than 100000 cases have red circles. Adjusted circle size according to the cases confirmed.

--- leaflet_corona_U.S..R
+++ leaflet_corona_U.S..R
@@ -7,8 +7,16 @@ library(tidyverse)
 
 covid <- read_csv("05-27-2020.csv")
 
-covid <- covid %>% rename(lng  = Long_, lat = Lat)
+covid <- covid %>% rename(lng  = Long_, lat = Lat) %>% mutate(severe = ifelse(covid$Confirmed >= 100000, TRUE, FALSE),
+                                                        circlesize = ifelse(covid$Confirmed <= 10000, 10,
+                                                                 ifelse(covid$Confirmed <= 30000, 15,
+                                                                 ifelse(covid$Confirmed <= 50000, 20,30
+                                                                                     ))))
+                                                                              
+                                                                              
+                                          
 
+pal <- colorFactor(c("blue", "red"), covid$severe)
 
 
 ui <- bootstrapPage(
@@ -16,7 +24,7 @@ ui <- bootstrapPage(
 leafletOutput('map', width = "200%", height = 1000) ,
 
 absolutePanel(top =10, right = 30, id = 'controls', 
-              sliderInput('cases', 'Number of cases', 1, 500000, 3000)
+              sliderInput('cases', 'Number of cases', 1, 500000, 5000)
   
 )
   
@@ -32,8 +40,9 @@ covid %>% filter(Confirmed <= input$cases) %>%
  leaflet() %>% addProviderTiles(providers$Stamen.Toner) %>%
   #fitBounds(~min(covid$Lat), ~min(covid$Long_), ~max(covid$Lat), ~max(covid$Long_)) %>%
   setView(-95.7129, 37.0902, zoom = 4) %>% addCircleMarkers( weight = ~(Deaths/1000),
-                                popup = paste(covid$Province_State, '\n', (as.character(covid$Confirmed)), 'cases, ',
-                                                        as.character(covid$Deaths), 'deaths')
+                                popup = ~paste(Province_State, '\n', (as.character(Confirmed)), 'cases, ',
+                                as.character(Deaths), 'deaths'), color = ~pal(severe),
+                                radius = ~circlesize
                                          
      )
 })
@@ -42,5 +51,9 @@ covid %>% filter(Confirmed <= input$cases) %>%
 
 
 shinyApp(ui, server)
+
+
+
+
 
 
