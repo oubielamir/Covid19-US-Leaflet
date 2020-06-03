5c26ba3fddb5e302a388ddb7901faac26e9049f5
oubielamir <oubielamir@Oubis-MacBook-Pro.local>
2020-06-03 12:38:08 -0400

    updated data for 06-02-2020. added comments to code.

--- leaflet_corona_U.S..R
+++ leaflet_corona_U.S..R
@@ -5,8 +5,8 @@ library(readr)
 library(tidyverse)
 
 
-covid <- read_csv("05-27-2020.csv")
-
+covid <- read_csv("06-02-2020.csv")
+#rename Long_ and Lat columns so Leaflet can recgonize the coordintes 
 covid <- covid %>% rename(lng  = Long_, lat = Lat) %>% mutate(severe = ifelse(covid$Confirmed >= 100000, TRUE, FALSE),
                                                         circlesize = ifelse(covid$Confirmed <= 10000, 10,
                                                                  ifelse(covid$Confirmed <= 30000, 15,
@@ -15,14 +15,15 @@ covid <- covid %>% rename(lng  = Long_, lat = Lat) %>% mutate(severe = ifelse(co
                                                                               
                                                                               
                                           
-
+#color factor states with high number of corona cases
 pal <- colorFactor(c("blue", "red"), covid$severe)
 
 
 ui <- bootstrapPage(
   
+#map layout  
 leafletOutput('map', width = "200%", height = 1000) ,
-
+#adding a slider input for users to view number of cases
 absolutePanel(top =10, right = 30, id = 'controls', 
               sliderInput('cases', 'Number of cases', 1, 500000, 5000)
   
@@ -35,10 +36,11 @@ absolutePanel(top =10, right = 30, id = 'controls',
 server <- function(input, output, session){
 
   output$map <-  renderLeaflet({
+#taking input from user
 covid %>% filter(Confirmed <= input$cases) %>%
-#%>% filter(Deaths <= 100) 
+ #adding tiles 
  leaflet() %>% addProviderTiles(providers$Stamen.Toner) %>%
-  #fitBounds(~min(covid$Lat), ~min(covid$Long_), ~max(covid$Lat), ~max(covid$Long_)) %>%
+  #set view for the US and circle markers
   setView(-95.7129, 37.0902, zoom = 4) %>% addCircleMarkers( weight = ~(Deaths/1000),
                                 popup = ~paste(Province_State, '\n', (as.character(Confirmed)), 'cases, ',
                                 as.character(Deaths), 'deaths'), color = ~pal(severe),
