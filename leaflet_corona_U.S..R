bf11e12688a6c16e8e4b331b8bfb6724823a6c42
oubielamir <oubielamir@Oubis-MacBook-Pro.local>
2020-06-29 14:56:40 -0400

    Updated data file for 06-28-2020. Added reactive variables and color coded the states 4 ways.

--- leaflet_corona_U.S..R
+++ leaflet_corona_U.S..R
@@ -2,60 +2,68 @@ library(shiny)
 library(leaflet)
 library(leaflet.providers)
 library(readr)
-library(tidyverse)
+library(dplyr)
+
+covid <- read_csv("06-28-2020.csv")
 
 
-covid <- read_csv("06-07-2020.csv")
-#rename Long_ and Lat columns so Leaflet can recgonize the coordintes 
-covid <- covid %>% rename(lng  = Long_, lat = Lat) %>% mutate(severe = ifelse(covid$Confirmed >= 100000, TRUE, FALSE),
-                                                        circlesize = ifelse(covid$Confirmed <= 10000, 10,
-                                                                 ifelse(covid$Confirmed <= 30000, 15,
-                                                                 ifelse(covid$Confirmed <= 50000, 20,30
-                                                                                     ))))
-                                                                              
-                                                                              
-                                          
+
+
+#naming longitude and latitude so leaflet can read the columns
+covid <- covid %>% rename(lng  = Long_, lat = Lat) %>% 
+
+                   mutate(severe = ifelse(covid$Confirmed >= 100000, TRUE, FALSE),
+                   circlesize = ifelse(covid$Confirmed <= 10000, 10,
+                                    ifelse(covid$Confirmed <= 30000, 15,
+                                       ifelse(covid$Confirmed <= 50000, 20,30
+                                       ))))
+
+
+
 #color factor states with high number of corona cases
-pal <- colorFactor(c("blue", "red"), covid$severe)
-
+binpal <- colorBin(c("blue", "purple", "orange", "red"), covid$Confirmed, bins = 4, pretty = F, reverse = F)
 
 ui <- bootstrapPage(
   
-#map layout  
-leafletOutput('map', width = "200%", height = 1000) ,
-#adding a slider input for users to view number of cases
-absolutePanel(top =10, right = 30, id = 'controls', 
-              sliderInput('cases', 'Number of cases', 1, 500000, 5000)
-  
-)
+  #map layout  
+  leafletOutput('map', width = "200%", height = 1000) ,
+  #adding a slider input for users to view number of cases
+  absolutePanel(top =10, right = 30, id = 'controls', 
+                sliderInput('cases', 'Number of cases', 1, 500000, 5000)
+                
+  )
   
 )
 
 
 
 server <- function(input, output, session){
-
+  
+  #Filter data
+  data <- reactive({
+    covid %>% filter(Confirmed <= input$cases)
+  })
+  
+  observe({
+    df = data()
+    leafletProxy("map", data = df) %>% 
+    addProviderTiles(providers$Stamen.Toner)
+      
+   })
   output$map <-  renderLeaflet({
-#taking input from user
-covid %>% filter(Confirmed <= input$cases) %>%
- #adding tiles 
- leaflet() %>% addProviderTiles(providers$Stamen.Toner) %>%
-  #set view for the US and circle markers
-  setView(-95.7129, 37.0902, zoom = 4) %>% addCircleMarkers( weight = ~(Deaths/1000),
-                                popup = ~paste(Province_State, '\n', (as.character(Confirmed)), 'cases, ',
-                                as.character(Deaths), 'deaths'), color = ~pal(severe),
-                                radius = ~circlesize
-                                         
-     )
-})
     
+      #adding tiles 
+     m <- data() %>% leaflet() %>% addProviderTiles(providers$Stamen.Toner) %>%
+      #set view for the US and circle markers
+      setView(-95.7129, 37.0902, zoom = 4) %>%
+      addCircleMarkers(weight = ~(Deaths/1000),
+                        label = ~paste(Province_State, '\n', (as.character(Confirmed)), 'cases, ',
+                                       as.character(Deaths), 'deaths'), color = ~binpal(covid$Confirmed),
+                        radius = ~circlesize ) 
+     m 
+  })
+  
 }
 
 
 shinyApp(ui, server)
-
-
-
-
-
-
